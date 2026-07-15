4ca8222d9d9d0ba129c012ad6db57b84d1914818
Martin Ergo <martinergo06@gmail.com>
2026-07-15 18:46:24 +0200

    initial commit

---
+++ src/main/java/dev/nitramnibus/papertest/PaperTestLoader.java
@@ -0,0 +1,12 @@
+package dev.nitramnibus.papertest;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class PaperTestLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
